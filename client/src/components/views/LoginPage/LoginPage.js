c22383dc2fa89daa2a04cf0e1fae882420a9bef0
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2020-12-19 14:11:11 +0900

    Fix let -> const

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,26 +1,28 @@
-import React, { useState } from "react";
-import { withRouter } from "react-router-dom";
-import { loginUser } from "../../../_actions/user_actions";
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { Form, Input, Button, Checkbox, Typography } from 'antd';
-import { UserOutlined, LockOutlined } from "@ant-design/icons";
-import { useDispatch } from "react-redux";
+import React, { useState } from 'react'
+import { withRouter } from 'react-router-dom'
+import { loginUser } from '../../../_actions/user_actions'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+import { Form, Input, Button, Checkbox, Typography } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import { useDispatch } from 'react-redux'
 
-const { Title } = Typography;
+const { Title } = Typography
 
 function LoginPage(props) {
-  const dispatch = useDispatch();
-  const rememberMeChecked = localStorage.getItem("rememberMe") ? true : false;
+  const dispatch = useDispatch()
+  const rememberMeChecked = localStorage.getItem('rememberMe') ? true : false
 
   const [formErrorMessage, setFormErrorMessage] = useState('')
   const [rememberMe, setRememberMe] = useState(rememberMeChecked)
 
   const handleRememberMe = () => {
     setRememberMe(!rememberMe)
-  };
+  }
 
-  const initialEmail = localStorage.getItem("rememberMe") ? localStorage.getItem("rememberMe") : '';
+  const initialEmail = localStorage.getItem('rememberMe')
+    ? localStorage.getItem('rememberMe')
+    : ''
 
   return (
     <Formik
@@ -38,36 +40,36 @@ function LoginPage(props) {
       })}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
-          let dataToSubmit = {
+          const dataToSubmit = {
             email: values.email,
-            password: values.password
-          };
+            password: values.password,
+          }
 
           dispatch(loginUser(dataToSubmit))
-            .then(response => {
+            .then((response) => {
               if (response.payload.loginSuccess) {
-                window.localStorage.setItem('userId', response.payload.userId);
+                window.localStorage.setItem('userId', response.payload.userId)
                 if (rememberMe === true) {
-                  window.localStorage.setItem('rememberMe', values.id);
+                  window.localStorage.setItem('rememberMe', values.id)
                 } else {
-                  localStorage.removeItem('rememberMe');
+                  localStorage.removeItem('rememberMe')
                 }
-                props.history.push("/");
+                props.history.push('/')
               } else {
                 setFormErrorMessage('Check out your Account or Password again')
               }
             })
-            .catch(err => {
+            .catch((err) => {
               setFormErrorMessage('Check out your Account or Password again')
               setTimeout(() => {
-                setFormErrorMessage("")
-              }, 3000);
-            });
-          setSubmitting(false);
-        }, 500);
+                setFormErrorMessage('')
+              }, 3000)
+            })
+          setSubmitting(false)
+        }, 500)
       }}
     >
-      {props => {
+      {(props) => {
         const {
           values,
           touched,
@@ -78,13 +80,11 @@ function LoginPage(props) {
           handleBlur,
           handleSubmit,
           handleReset,
-        } = props;
+        } = props
         return (
           <div className="app">
-
             <Title level={2}>Log In</Title>
             <form onSubmit={handleSubmit} style={{ width: '350px' }}>
-
               <Form.Item required>
                 <Input
                   id="email"
@@ -95,7 +95,9 @@ function LoginPage(props) {
                   onChange={handleChange}
                   onBlur={handleBlur}
                   className={
-                    errors.email && touched.email ? 'text-input error' : 'text-input'
+                    errors.email && touched.email
+                      ? 'text-input error'
+                      : 'text-input'
                   }
                 />
                 {errors.email && touched.email && (
@@ -113,7 +115,9 @@ function LoginPage(props) {
                   onChange={handleChange}
                   onBlur={handleBlur}
                   className={
-                    errors.password && touched.password ? 'text-input error' : 'text-input'
+                    errors.password && touched.password
+                      ? 'text-input error'
+                      : 'text-input'
                   }
                 />
                 {errors.password && touched.password && (
@@ -122,29 +126,56 @@ function LoginPage(props) {
               </Form.Item>
 
               {formErrorMessage && (
-                <label ><p style={{ color: '#ff0000bf', fontSize: '0.7rem', border: '1px solid', padding: '1rem', borderRadius: '10px' }}>{formErrorMessage}</p></label>
+                <label>
+                  <p
+                    style={{
+                      color: '#ff0000bf',
+                      fontSize: '0.7rem',
+                      border: '1px solid',
+                      padding: '1rem',
+                      borderRadius: '10px',
+                    }}
+                  >
+                    {formErrorMessage}
+                  </p>
+                </label>
               )}
 
               <Form.Item>
-                <Checkbox id="rememberMe" onChange={handleRememberMe} checked={rememberMe} >Remember me</Checkbox>
-                <a className="login-form-forgot" href="/reset_user" style={{ float: 'right' }}>
+                <Checkbox
+                  id="rememberMe"
+                  onChange={handleRememberMe}
+                  checked={rememberMe}
+                >
+                  Remember me
+                </Checkbox>
+                <a
+                  className="login-form-forgot"
+                  href="/reset_user"
+                  style={{ float: 'right' }}
+                >
                   forgot password
-                  </a>
+                </a>
                 <div>
-                  <Button type="primary" htmlType="submit" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    className="login-form-button"
+                    style={{ minWidth: '100%' }}
+                    disabled={isSubmitting}
+                    onSubmit={handleSubmit}
+                  >
                     Log in
-                </Button>
+                  </Button>
                 </div>
                 Or <a href="/register">register now!</a>
               </Form.Item>
             </form>
           </div>
-        );
+        )
       }}
     </Formik>
-  );
-};
+  )
+}
 
-export default withRouter(LoginPage);
-
-
+export default withRouter(LoginPage)
