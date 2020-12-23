7380d4792910646a4abdf894c6eff30e80f2cfb1
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2020-12-23 09:53:30 +0900

    Add Cart 담기 로직 구현

--- server/routes/users.js
+++ server/routes/users.js
@@ -20,6 +20,9 @@ router.get('/auth', auth, (req, res) => {
     lastname: req.user.lastname,
     role: req.user.role,
     image: req.user.image,
+    // cart 와 history 정보도 auth 미들웨어를 통과할 때 마다 상태를 가지고 있어야 함.
+    cart: req.user.cart,
+    history: req.user.history,
   })
 })
 
@@ -82,6 +85,65 @@ router.get('/logout', auth, (req, res) => {
       })
     }
   )
+})
+
+router.post('/addToCart', auth, (req, res) => {
+  // 먼저 해당 유저의 정보를 다 가져옴 (req.user._id 사용할 수 있는 이유는 auth 미들웨어 때문)
+  User.findOne({ _id: req.user._id }, (err, userInfo) => {
+    // userInfo 는 User 모델의 document 이다.
+    // 가져온 정보에서 Cart 에 넣으려 하는 상품이 이미 들어있는지 확인
+    let duplicate = false
+    userInfo.cart.forEach((item) => {
+      if (item.id === req.body.productId) {
+        duplicate = true
+      }
+    })
+
+    // 상품이 카트에 이미 있을 때
+    if (duplicate) {
+      User.findOneAndUpdate(
+        {
+          _id: req.user._id,
+          'cart.id': req.body.productId, // 먼저 해당 User를 찾고 그 User의 cart array의 id가 productId 인 것을 찾는다.
+        },
+        {
+          $inc: { 'cart.$.quantity': 1 }, // $inc는 증가시킨 다는 의미, cart의 quantity를 1 증가 시킨다.
+        },
+        {
+          new: true, // 업데이트 된 정보를 받기위해 이 옵션이 필요하다.
+        },
+        (err, userInfo) => {
+          // new: true 옵션을 줬기 때문에 userInfo에 업데이트 된 정보가 들어있다.
+          if (err) return res.status(400).json({ success: false, err })
+          res.status(200).send(userInfo.cart) // cart의 정보만 보내준다. (리듀서의 payload 로 들어감)
+        }
+      )
+    } else {
+      // 상품이 카트에 없을 때
+      User.findOneAndUpdate(
+        {
+          _id: req.user._id,
+        },
+        {
+          $push: {
+            // cart 부분에 넣어준다.
+            cart: {
+              id: req.body.productId,
+              quantity: 1,
+              date: Date.now(),
+            },
+          },
+        },
+        {
+          new: true, // 업데이트 된 정보를 받기위해 이 옵션이 필요하다.
+        },
+        (err, userInfo) => {
+          if (err) return res.status(400).json({ success: false, err })
+          res.status(200).send(userInfo.cart) // cart의 정보만 보내준다.
+        }
+      )
+    }
+  })
 })
 
 module.exports = router
